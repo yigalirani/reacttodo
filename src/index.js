c2b3c62458e27df9dd0e047a2fcddfcbfaca3b2a
Yigal Irani <yigal@symbolclick.com>
2019-07-03 04:30:03 -0400

    switched to use model. starts working, bug with key

--- src/index.js
+++ src/index.js
@@ -35,105 +35,18 @@ function BlurInput(props){//className,value
 		if (e.which === 27) {//escape key
 			props.onBlur(text,false) //false, dont update the data
 		}		
-
 	}
 	return <input {...props} value={text} type="text" {...{onBlur,onChange,onKeyUp}} autoFocus="yes"/>
 }
-function TodoItem({item,onToggle,ondestroy,onChange}){
-	var checkbox_props={onChange:_=>{onToggle(item.key)},checked:''}
-	var [editing,setEditing]=useState('')
-	var li_props={className:editing}
-	if (item.completed){
-		checkbox_props.checked='checked';
-		li_props.className+=' completed'
+function itemsModel([list,setList]){
+	//var [list,setList]=useState([])
+	var key=Math.max(list.map(x=>x.key))+1
+	function load(){
+		var loaded_list=JSON.parse(localStorage.getItem('todo'))||[]
+		setList(loaded_list)
 	}
-	function onBlur(tx,shuld_update){
-		setEditing('') 
-		if (shuld_update)
-			onChange(tx,item.key)
-	}
-	function destory_clicked(){
-		ondestroy(item.key)
-	}
-	return 	<li {...li_props}>
-				<div className="view">
-					<input className="toggle" type="checkbox" {...checkbox_props}/>
-					<label onDoubleClick={x=>setEditing('editing')}>{item.tx}</label>
-					<button className="destroy" onClick={destory_clicked}></button>
-				</div>
-				<BlurInput className="edit" inital_text={item.tx} onBlur={onBlur}  />
-			</li>
-}
-
-function TodoList({list,stab,onToggle,onChange,ondestroy}){
-	var filters={
-		active:x=>!x.completed,
-		completed:x=>x.completed
-	}
-	var filtered=list.filter(filters[stab]||(x=>true))
-	return <ul className='todo-list'>{filtered.map(x=><TodoItem item={x} key={x.key} {...{onToggle,onChange,ondestroy}}/>) }</ul>
-}
-function cp(a,b){
-	return Object.assign({}, a,b);
-}
-function eq(a,b){
-	return (a.toLowerCase()===b.toLowerCase())
-}
-function update_list(list,key,cb){
-	var index=list.findIndex(x=>{return x.key==key})
-	if (index==-1)
-		return list
-	var item=list[index]
-	cb(item)
-	return [...list.slice(0,index),item,...list.slice(index+1)]
-}
-function Tab({tab,stab,setStab}){
-	var a={}
-	if (eq(tab,stab))
-		a.className='selected'
-	return <li><a href={'#'+tab} {...a} onClick={x=>setStab(tab.toLowerCase())}>{tab}</a> </li>
-}
-function activeTodoCount(list){
-	var ans=list.filter(x=>!x.completed).length
-	if (ans==1)
-		return '1 item'
-	return ans+' items'
-}
-function Footer({list,stab,setStab,clear_completed,tab}){
-	if (list.length==0)
-		return ''
-	var link_props={stab,setStab}
-	return <footer className="footer">
-	<span className="todo-count"><strong>{activeTodoCount(list)}</strong> left</span>
-			<ul  className="filters">
-				{['All','Active','Completed'].map(x=><Tab tab={x} key={x} {...link_props}/> )}
-			</ul>
-			<button className="clear-completed" onClick={clear_completed}>Clear completed</button>
-	</footer>
-}
-function calcFilter() {
-	return document.location.hash.toLowerCase().substring(1)
-}
-
-function load(){
-	return JSON.parse(localStorage.getItem('todo'))
-}
-function TodoApp(){
-	var [list,setList]=useState([])
-	var [key,setKey]=useState(1)
-	var [stab,setStab]=useState(calcFilter())//seed the state from url
-	function save(data){
-		localStorage.setItem('todo', JSON.stringify(data));
-		setList(data)
-	}	
-	useEffect(_=>{ //mu ha ha, saving state without using react!!
-		var the_list=load()||[]
-		setList(the_list)
-		setKey(Math.max(the_list.map(x=>x.key))+1)
-		return x=>save(list)
-	},[])
 	function append_item(tx){
-		setKey(key+1)
+		key+=1
 		var new_list=list.concat([{tx,key,completed:false}])
 		save(new_list)
 	
@@ -158,20 +71,115 @@ function TodoApp(){
 	}
 	function onChange(tx,key){
 		save(update_list(list,key,item=>item.tx=tx))
-
+	}	
+	function update_list(list,key,cb){
+		var index=list.findIndex(x=>{return x.key==key})
+		if (index==-1)
+			return list
+		var item=list[index]
+		cb(item)
+		return [...list.slice(0,index),item,...list.slice(index+1)]
 	}
+	function save(new_list){
+		localStorage.setItem('todo', JSON.stringify(new_list||list));
+		setList(new_list)
+	}
+
+	return {list,ondestroy,onToggle,clear_completed,toggle_all,onChange,load,save,append_item}	
+
+}
+function TodoItem({item,model}){
+	var checkbox_props={onChange:_=>{model.onToggle(item.key)},checked:''}
+	var [editing,setEditing]=useState('')
+	var li_props={className:editing}
+	if (item.completed){
+		checkbox_props.checked='checked';
+		li_props.className+=' completed'
+	}
+	function onBlur(tx,shuld_update){
+		setEditing('') 
+		if (shuld_update)
+			model.onChange(tx,item.key)
+	}
+	function destory_clicked(){
+		model.ondestroy(item.key)
+	}
+	function destroy_item(){
+		model.ondestroy(item.key)
+	}
+	return 	<li {...li_props}>
+				<div className="view">
+					<input className="toggle" type="checkbox" {...checkbox_props}/>
+					<label onDoubleClick={x=>setEditing('editing')}>{item.tx}</label>
+					<button className="destroy" onClick={destroy_item}></button>
+				</div>
+				<BlurInput className="edit" inital_text={item.tx} onBlur={onBlur}  />
+			</li>
+}
+
+function TodoList({stab,model}){
+	var filters={
+		active:x=>!x.completed,
+		completed:x=>x.completed
+	}
+	var filtered=model.list.filter(filters[stab]||(x=>true))
+	return <ul className='todo-list'>{filtered.map(x=><TodoItem item={x} key={x.key} {...{model}}/>) }</ul>
+}
+function cp(a,b){
+	return Object.assign({}, a,b);
+}
+function eq(a,b){
+	return (a.toLowerCase()===b.toLowerCase())
+}
+function Tab({tab,stab,setStab}){
+	var a={}
+	if (eq(tab,stab))
+		a.className='selected'
+	return <li><a href={'#'+tab} {...a} onClick={x=>setStab(tab.toLowerCase())}>{tab}</a> </li>
+}
+function activeTodoCount(list){
+	var ans=list.filter(x=>!x.completed).length
+	if (ans==1)
+		return '1 item'
+	return ans+' items'
+}
+function Footer({setStab,model,stab}){
+	if (model.list.length==0)
+		return ''
+	var link_props={stab,setStab}
+	return <footer className="footer">
+	<span className="todo-count"><strong>{activeTodoCount(model.list)}</strong> left</span>
+			<ul  className="filters">
+				{['All','Active','Completed'].map(x=><Tab tab={x} key={x} {...link_props}/> )}
+			</ul>
+			<button className="clear-completed" onClick={model.clear_completed}>Clear completed</button>
+	</footer>
+}
+function calcFilter() {
+	return document.location.hash.toLowerCase().substring(1)
+}
+
+function TodoApp(){
+	var model=itemsModel(useState([]))
+	var [stab,setStab]=useState(calcFilter())//seed the state from url
+
+	useEffect(_=>{ //mu ha ha, saving state without using react!!
+		model.load()
+		return x=>model.save()
+	},[])
+
 	var checked={checked:'checked'}
 	return <section className='todoapp'>
 			<header className='header'>
 				<h1>todos</h1>
-				<Input  onEnter={append_item}/>
+				<Input  onEnter={model.append_item}/>
 			</header>
 			<section className="main">
-				<input className="toggle-all" type="checkbox" onChange={toggle_all} checked/>
-				<label htmlFor="toggle-all" onClick={toggle_all} >Mark all as complete  </label>
-				<TodoList {...{list,stab,onToggle,onChange,ondestroy}}/>
+				<input className="toggle-all" type="checkbox" onChange={model.toggle_all} checked/>
+				<label htmlFor="toggle-all" onClick={model.toggle_all} >Mark all as complete  </label>
+				<TodoList {...{stab,model}}/>
 			</section>
-			<Footer {...{list,setStab,clear_completed,stab}}/>		
+			<Footer {...{setStab,model,stab}}/>		
 		</section>
 }//just diccovered this trik: {...{
 ReactDOM.render(
